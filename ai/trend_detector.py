b81cd20d179af3f7508a7c4923011674e79fe93e
Dentara <drsiatagizade@gmail.com>
2025-07-25 14:54:29 +0400

    Update trend_detector.py

--- ai/trend_detector.py
+++ ai/trend_detector.py
@@ -6,15 +6,15 @@ class TrendDetector:
 
     def detect_trend(self, close_prices: list[float]) -> str:
         if len(close_prices) < self.candle_count:
-            return \"neutral\"
+            return "neutral"
 
         recent = close_prices[-self.candle_count:]
         up = all(recent[i] < recent[i + 1] for i in range(len(recent) - 1))
         down = all(recent[i] > recent[i + 1] for i in range(len(recent) - 1))
 
         if up:
-            return \"uptrend\"
+            return "uptrend"
         elif down:
-            return \"downtrend\"
+            return "downtrend"
         else:
-            return \"range\"
+            return "range"
